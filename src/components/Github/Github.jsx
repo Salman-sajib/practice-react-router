ff79f26673c55da23d6cb42642ceec5b3be8fcb4
Salman-sajib <sfsajib@gmail.com>
2024-02-07 17:38:33 +0600

    finished the github component

---
+++ src/components/Github/Github.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Github() {
+  return (
+    <div>
+      <h1 className='text-3xl text-orange-600 p-12'>
+        Sorry, Nothin is in here.....
+      </h1>
+    </div>
+  );
+}
+
+export default Github;
