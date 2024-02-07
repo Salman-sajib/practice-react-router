92f81db150c6275d381ff8b680900cad4447bb7e
Salman-sajib <sfsajib@gmail.com>
2024-02-07 21:21:29 +0600

    added a new component called user

---
+++ src/components/User/User.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function user() {
+  return <div className='bg-orange-500 text-3xl text-center py-5'>user</div>;
+}
+
+export default user;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import App from './App.jsx';
 import './index.css';
 
 import {
@@ -15,6 +14,7 @@ import Home from './components/Home/Home.jsx';
 import About from './components/About/About.jsx';
 import Contact from './components/Contact/Contact.jsx';
 import Github from './components/Github/Github.jsx';
+import User from './components/User/user.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -23,6 +23,7 @@ const router = createBrowserRouter(
       <Route path='about' element={<About />} />
       <Route path='contact' element={<Contact />} />
       <Route path='github' element={<Github />} />
+      <Route path='user/:userid' element={<User />} />
     </Route>
   )
 );
