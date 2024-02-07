7ae8afe6123cc72bb37d6d8c191c057afd8b1bfd
Salman-sajib <sfsajib@gmail.com>
2024-02-07 21:31:15 +0600

    learned how to set a path dynamically and extract the value from it

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 function user() {
-  return <div className='bg-orange-500 text-3xl text-center py-5'>user</div>;
+  const { userid } = useParams();
+
+  return (
+    <div className='bg-orange-500 text-3xl text-center py-5'>
+      user: {userid}{' '}
+    </div>
+  );
 }
 
 export default user;
